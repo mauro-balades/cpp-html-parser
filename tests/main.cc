df8cfb630780396a013e8044a8f8e9e053f54ba0
mauro-balades <mauro.balades@tutanota.com>
2022-03-30 17:40:50 +0200

    removed googletests

--- tests/main.cc
+++ tests/main.cc
@@ -1,10 +1,12 @@
 
 #include <gtest/gtest.h>
+#include <HTMLParser/HTMLParser.h>
 
 class BasicsTest : public ::testing::Test {
 };
 
 TEST_F(BasicsTest, TestBackwardsCompat) {
 
+    
     ASSERT_TRUE(true);
 }
