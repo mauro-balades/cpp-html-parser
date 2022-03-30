d3702d3142094a3adfec2b7645f606913575111f
mauro-balades <mauro.balades@tutanota.com>
2022-03-30 17:45:02 +0200

    removed C make tests list

--- CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mauro/work/cpp-html-parser
-# Build directory: /home/mauro/work/cpp-html-parser
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("third_party/googletest")
-subdirs("tests")
