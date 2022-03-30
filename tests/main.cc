138cd483e5e2f1df0d59e736a3356c5f3a250682
mauro-balades <mauro.balades@tutanota.com>
2022-03-30 18:00:59 +0200

    fixed liker problem in tests

--- tests/main.cc
+++ tests/main.cc
@@ -6,7 +6,8 @@ class BasicsTest : public ::testing::Test {
 };
 
 TEST_F(BasicsTest, TestBackwardsCompat) {
+    std::string code = "<h1>hello</h1>";
 
-    
+    HTMLParser::Parser* parser = new HTMLParser::Parser(code);
     ASSERT_TRUE(true);
 }
