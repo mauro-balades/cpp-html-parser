46cb3da360475495788a7ededa246440f41c4887
mauro-balades <mauro.balades@tutanota.com>
2022-04-02 18:35:37 +0200

    added simple parsing for html elements (WIP)

--- source/HTMLParser.cc
+++ source/HTMLParser.cc
@@ -3,6 +3,11 @@
 #include "HTMLParser/HTMLDocument.h"
 #include "HTMLParser/HTMLTokenizer.h"
 
+#define GET_NEXT_TOKEN()             \
+    i++;                             \
+    Token current_token = tokens[i]; \
+
+
 namespace HTMLParser {
     class Tokenizer;
 
@@ -16,5 +21,25 @@ namespace HTMLParser {
 
     void Parser::parse() {
         tokenize();
+
+        std::vector<Token> tokens = _tokenizer->get_tokens();
+        for(std::vector<T>::size_type i = 0; i != tokens.size(); i++) {
+            Token current_token = tokens[i];
+
+            switch (token.type) {
+
+                case TokenType::OTAG: {
+
+                    GET_NEXT_TOKEN();
+                    if (current_token.type == TokenType::IDNT) { // <idnt>
+
+                    } else if (current_token.type == TokenType::CTAG) { // <>
+
+                    } else { // ??
+                        // TODO: throw any error?
+                    }
+                };
+            }
+        }
     }
 }
